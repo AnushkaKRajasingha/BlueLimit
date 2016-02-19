ab74bd944991a96073e002d1222781d17a7899fe
Anushka Rajasingha <AnushkaKRajasingha@users.noreply.github.com>
2016-02-19 13:09:09 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plugin Name: BlueLimit
+ * Plugin URI: http://www.wpbluelimit.com/
+ * Description: Wordpress Plugin that can prevent / block unnecessary contents
+ * Version: 0.0.0.1
+ * Db Version: 0.0.0.1
+ * Db Remove: No
+ * Author: Anushka K R
+ * Author URI: http://www.anushkar.com/
+ * License: bluelimit
+ * Text Domain: bl
+ * Icon URL: icon.png
+ * License Srv Url: http://www.wpbluelimit.com
+ * License Secert: free-version
+ * UserDocumentation: http://doc.wpbluelimit.com
+ * DevDocumentation: http://devapi.wpbluelimit.com
+ * HelpAndSupport: http://support.wpbluelimit.com
+ * Environment: Dev
+ * 
+ * 
+ * Change Log:
+ * 1.0.0 - First Commit
+ */
+
+?>
